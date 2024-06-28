121fd3885705ffeebe4722ccffb241ef7df0d79b
Ashok Velpula <ashokvelpula@Ashoks-MacBook-Pro.local>
2024-06-28 14:48:53 +1000

    first commit

--- app.py
+++ app.py
@@ -19,3 +19,19 @@ my_list = [0, 1, 2, 3, 4, 5, 6]
 slice_1 = my_list[1:4]
 print(slice_1)  # Output: [1, 2, 3, 4]
 
+## Tuples are ordered, immutable collections that can contain elements of different types.
+
+set = (1,2,3,4,5,6,7,8,9,10);
+print(set);
+print(type(set));
+
+## Dictionaries are unordered, mutable collections of key-value pairs, where keys are unique and immutable,
+
+set = {'name': 'Ashok', 'age': 25, 'city': 'Bangal'};
+print(set);
+print(type(set))         ##  what type it is
+print(set['name']);      ## accessing the individual elements
+print(set['age']);
+set['country'] = 'india'    ## adding a value 
+print(set);
+
---
+++ operators.py
@@ -0,0 +1,173 @@
+## example of variable change
+
+# Integer variable
+age = 30
+print(age)  # 30
+
+# String variable
+name = "Alice"
+print(name)  # Alice
+
+# Float variable
+price = 19.99
+print(price)  # 19.99
+
+# Boolean variable
+is_member = True
+print(is_member)  # True
+
+# List variable
+colors = ["red", "green", "blue"]
+print(colors)  # ['red', 'green', 'blue']
+
+# Dictionary variable
+person = {"name": "John", "age": 30}
+print(person)  # {'name': 'John', 'age': 30}
+
+
+
+
+
+### In Python, operators are special symbols or keywords that perform operations on values and variables. 
+###Here is a breakdown of the different types of operators
+
+
+# Arithmetic Operators  like +, -, *, /, %, **, //
+
+## examples
+## addition
+x = 10;
+y =20;
+Result = x + y;
+print(Result);
+print(type(Result));
+
+## subtraction
+
+x = 10;
+y = 20;
+Result = x - y;
+print(Result);
+print(type(Result));
+
+## multiplication
+
+x = 10;
+y = 20;
+Result = x * y;
+print(Result);
+print(type(Result));
+
+## division
+
+x = 10;
+y = 20;
+Result = x / y;
+print(Result);
+print(type(Result));
+
+## modulus
+
+x = 10;
+y = 20;
+Result = x % y;
+print(Result);
+print(type(Result));
+
+## power
+
+x = 10;
+y = 20;
+Result = x ** y;
+print(Result);
+print(type(Result));
+
+## floor division
+
+x = 10;
+y = 20;
+Result = x // y;
+print(Result);
+print(type(Result));
+
+##Comparison Operators for comparing two values like ==, !=, >, <, >=, <=
+## Equal to
+x = 10;
+y = 20;
+Result = x == y;
+print(Result);
+print(type(Result));
+
+
+## Not equal to
+
+x =10;
+y =20;
+Result = x != y;
+print(Result);
+print(type(Result));
+
+## Greater than
+
+x = 10;
+y = 20;
+Result = x > y;
+print(Result);
+print(type(Result));
+
+## Less than
+
+x = 10;
+y = 20;
+Result = x < y;
+print(Result);
+print(type(Result));
+
+## Greater than or equal to
+
+x = 10;
+y = 20;
+Result = x >= y;
+print(Result);
+print(type(Result));
+
+## Less than or equal to
+
+x = 10;
+y = 20;
+Result = x <= y;
+print(Result);
+print(type(Result));
+
+## Logical Operators for combining conditional statements like and, or, not
+
+## Logical AND
+
+x = True;
+y = False;
+Result = x and y;
+print(Result);
+print(type(Result));
+
+## Logical OR
+
+x = True;
+y = False;
+Result = x or y;
+print(Result);
+print(type(Result));
+
+## Logical NOT
+
+x = True;
+y = False;
+Result = not x;
+print(Result);
+print(type(Result));
+
+
+
+
+
+
+
